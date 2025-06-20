b2e3c0a3253624a519572f698f94661d7997d149
banele2025 <hlophephilile682@gmail.com>
2025-06-20 16:10:00 +0200

    Added Module 11 Python files

---
+++ Module11/strings.py
@@ -0,0 +1,10 @@
+#Advanced concept-strings
+
+message = ' Hello, World! '
+
+print(message.strip()) # Remove leading and trailing whitespace
+print(message.lower()) # Convet all characters to lowercase
+print(message.split(',')) # Split the strings into a list based on the comma
+
+#upper method
+#replace method
